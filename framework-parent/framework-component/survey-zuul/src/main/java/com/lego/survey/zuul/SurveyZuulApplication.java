d7c4599b2e55b629e2e23db063535e3eb6944146
yanglf <899117>
2019-07-25 15:06:59 +0800

    1

--- framework-parent/framework-component/survey-zuul/src/main/java/com/lego/survey/zuul/SurveyZuulApplication.java
+++ framework-parent/framework-component/survey-zuul/src/main/java/com/lego/survey/zuul/SurveyZuulApplication.java
@@ -1,10 +1,14 @@
 package com.lego.survey.zuul;
+
+import com.lego.survey.zuul.custom.CustomShutdown;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "com.lego.survey")
 @EnableDiscoveryClient
@@ -15,6 +19,19 @@ public class SurveyZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SurveyZuulApplication.class, args);
+    }
+
+
+    @Bean
+    public CustomShutdown customShutdown() {
+        return new CustomShutdown();
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(CustomShutdown customShutdown) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers(customShutdown);
+        return factory;
     }
 
 }
